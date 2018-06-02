ab6c4e037c98c2326c35daa0c240c20747f8ac45
Dragan Milic <dragan@netice9.com>
2018-06-02 21:03:56 +0100

    split out open test

--- mmf_suite_test.go
+++ mmf_suite_test.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/draganm/mmf"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -30,78 +29,4 @@ var _ = AfterEach(func() {
 	if dir != "" {
 		Expect(os.RemoveAll(dir)).To(Succeed())
 	}
-})
-
-var _ = Describe("Open", func() {
-	var e error
-	var f *mmf.File
-	JustBeforeEach(func() {
-		f, e = mmf.Open(fileName)
-	})
-
-	Context("When the file does not exist", func() {
-
-		It("should not return an error", func() {
-			Expect(e).ToNot(HaveOccurred())
-		})
-
-		It("Should return a non-nil file", func() {
-			Expect(f).ToNot(BeNil())
-		})
-
-		It("Should create the file on the filesystem", func() {
-			fs, err := os.Stat(fileName)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(fs.IsDir()).To(BeFalse())
-			Expect(fs.Size()).To(Equal(int64(0)))
-		})
-
-	})
-
-	Context("When the file exists and is empty", func() {
-
-		BeforeEach(func() {
-			f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0700)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(f.Close()).To(Succeed())
-		})
-
-		It("should not return an error", func() {
-			Expect(e).ToNot(HaveOccurred())
-		})
-
-		It("Should return a non-nil file", func() {
-			Expect(f).ToNot(BeNil())
-		})
-
-		It("Should have size 0 mmap", func() {
-			Expect(len(f.MMap)).To(Equal(0))
-		})
-
-	})
-
-	Context("When the file exists and has some data", func() {
-
-		BeforeEach(func() {
-			f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0700)
-			Expect(err).ToNot(HaveOccurred())
-			_, err = f.Write([]byte{1, 2, 3})
-			Expect(err).ToNot(HaveOccurred())
-			Expect(f.Close()).To(Succeed())
-		})
-
-		It("should not return an error", func() {
-			Expect(e).ToNot(HaveOccurred())
-		})
-
-		It("Should return a non-nil file", func() {
-			Expect(f).ToNot(BeNil())
-		})
-
-		It("Should mmap the file's data", func() {
-			Expect([]byte(f.MMap)).To(Equal([]byte{1, 2, 3}))
-		})
-
-	})
-
 })
