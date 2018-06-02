c89c0c3206f417851db1435c2d31b82bdabd1846
Dragan Milic <dragan@netice9.com>
2018-06-02 21:17:59 +0100

    added write

--- file.go
+++ file.go
@@ -2,6 +2,8 @@ package mmf
 
 import (
 	"os"
+	"reflect"
+	"unsafe"
 
 	"github.com/tysontate/gommap"
 )
@@ -24,21 +26,55 @@ func Open(fileName string) (*File, error) {
 		return nil, err
 	}
 
-	mmap := gommap.MMap{}
+	file := &File{
+		f: f,
+	}
 
 	if stat.Size() != 0 {
-		mmap, err = gommap.Map(f.Fd(), gommap.PROT_READ, gommap.MAP_SHARED)
-		if err != nil {
-			return nil, err
-		}
-		err = mmap.Advise(gommap.MADV_RANDOM)
+		err = file.mmap()
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	return &File{
-		f:    f,
-		MMap: mmap,
-	}, nil
+	return file, nil
+}
+
+func (f *File) mmap() error {
+	mmap, err := gommap.Map(f.f.Fd(), gommap.PROT_READ, gommap.MAP_SHARED)
+	if err != nil {
+		return err
+	}
+	err = mmap.Advise(gommap.MADV_RANDOM)
+	if err != nil {
+		return err
+	}
+	f.MMap = mmap
+	return nil
+}
+
+// Append appends the data at the end of the file and extens the boundaries of the mmap
+func (f *File) Append(data []byte) error {
+
+	shouldMMAP := len(f.MMap) == 0
+
+	n, err := f.f.Write(data)
+
+	if err != nil {
+		return err
+	}
+
+	newLength := len(f.MMap) + n
+
+	// really uggly, but not to be avoided :(
+	dh := (*reflect.SliceHeader)(unsafe.Pointer(&f.MMap))
+	dh.Len = newLength
+	dh.Cap = newLength
+
+	if shouldMMAP {
+		err = f.mmap()
+	}
+
+	return err
+
 }
---
+++ write_test.go
@@ -0,0 +1,72 @@
+package mmf_test
+
+import (
+	"os"
+
+	"github.com/draganm/mmf"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Write", func() {
+	var e error
+	var f *mmf.File
+	JustBeforeEach(func() {
+		f, e = mmf.Open(fileName)
+	})
+
+	JustBeforeEach(func() {
+		e = f.Append([]byte{4, 5, 6})
+	})
+
+	Context("When the file did not exist before opening", func() {
+
+		It("should not return error", func() {
+			Expect(e).ToNot(HaveOccurred())
+		})
+
+		It("Should extend mmap to contain the appended data", func() {
+			Expect([]byte(f.MMap)).To(Equal([]byte{4, 5, 6}))
+		})
+
+	})
+
+	Context("When the file exists and is empty", func() {
+
+		BeforeEach(func() {
+			f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0700)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(f.Close()).To(Succeed())
+		})
+
+		It("should not return error", func() {
+			Expect(e).ToNot(HaveOccurred())
+		})
+
+		It("Should extend mmap to contain the appended data", func() {
+			Expect([]byte(f.MMap)).To(Equal([]byte{4, 5, 6}))
+		})
+
+	})
+
+	Context("When the file exists and has some data", func() {
+
+		BeforeEach(func() {
+			f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0700)
+			Expect(err).ToNot(HaveOccurred())
+			_, err = f.Write([]byte{1, 2, 3})
+			Expect(err).ToNot(HaveOccurred())
+			Expect(f.Close()).To(Succeed())
+		})
+
+		It("should not return error", func() {
+			Expect(e).ToNot(HaveOccurred())
+		})
+
+		It("Should extend mmap to contain the appended data", func() {
+			Expect([]byte(f.MMap)).To(Equal([]byte{1, 2, 3, 4, 5, 6}))
+		})
+
+	})
+
+})
